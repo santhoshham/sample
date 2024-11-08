0981d78e0956824c93626c8171d8bc878575c005
santhoshham <sandysanthoshgo007@gmail.com>
2024-11-08 23:45:22 +0530

    second stage

---
+++ src/Assets/Images.js
@@ -0,0 +1,11 @@
+import icon from './icon.png'
+import logo from './logo.png'
+import basicon from './basicon.png'
+
+ const Images = {
+   icon,
+   logo,
+   basicon
+}
+export default Images;
+
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import './Navbar.css'
+import images from '../Assets/Images';
+
  export default function Navbar(){
        return(
         <div className='Logo'>
-        dfgngfngg
+        <img src={images.logo} alt='Screen' className='Logo' />
+        <ul className='navbar-menu'>
+            <li>Home</li>
+            <li>Menu</li>
+            <li>Mobile-APP</li>
+            <li>Contact-US</li>
+         </ul> 
+         <div className='navbar-right'>
+            <img src={images.icon} alt='Search icon' />
+            <div className='navbar-search icon'>
+            <img src={images.basicon} alt='Basket icon'/>
+            </div>
+         </div>
     </div>
     );
 }
