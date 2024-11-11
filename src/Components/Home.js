4a7c4e6c8f00e401c82234b3479aded10ca0a0f0
santhoshham <sandysanthoshgo007@gmail.com>
2024-11-11 22:13:40 +0530

    "Headache starts"

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import './Home.css'
+import Header from '../Header/Header'
 export default function Home(){
     return(
     <div>
-
+    <Header/>
     </div>
     )
 }
---
+++ src/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+import headerImg from '../Assets/header_img.png';
+
+export default function Header(){
+    return(
+    <div className='header'  style={{ backgroundImage: `url(${headerImg})`, backgroundRepeat: 'no-repeat', }}>
+     <div className='Header-contents'>
+        <h2>Order your favourite here</h2>
+        <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients.</p>
+        <button>View Menu</button>
+     </div>
+    </div>
+    )
+}
