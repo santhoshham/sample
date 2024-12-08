91005b93dee8c10dcff10d830bda2f82db3c665f
santhoshham <sandysanthoshgo007@gmail.com>
2024-12-08 22:40:02 +0530

    UP

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,15 +1,35 @@
 import React, { useState } from 'react'
 import './Login.css'
-function Login({setShowLogin}){
+import { FaSkullCrossbones } from "react-icons/fa6";
+//import { RxCross1 } from "react-icons/rx";
 
-    const[current, setCurrent]=useState("Sign Up")
-    return( 
+
+function Login({ setShowLogin }) {
+
+    const [current, setCurrent] = useState("Login")
+    return (
         <div className='Login-popup'>
-           <form className='login-popup-container'>
-            <div className='login-popup-title'>
-                <h2>{current}</h2>
-            </div>
-           </form>
+            <form className='login-popup-container'>
+                <div className='login-popup-title'>
+                    <h2>{current}</h2>
+                    <FaSkullCrossbones size={20} color='red' onClick={() => setShowLogin(false)} />
+                </div>
+                <div className='login-popup-inputs'>
+                    {current === "Login" ? <></> : <input type="text" placeholder='Your name' required />}
+                    <input type="email" placeholder='Your email' required />
+                    <input type="password" placeholder='Your password' required />
+                    
+                </div>
+                <button>{current === "Sign Up" ? "Create account" : "Login"}</button>
+                <div className='login-popup-condition'>
+                    <input type='checkbox' required/>
+                    <p>By continuing, i agree to the terms of use & privacy policy.</p>
+                </div>
+                {current==="Login"
+                ?<p>Create a new account?<span onClick={()=>setCurrent("Sign Up")}>Click Here</span> </p>
+                :<p>Already have an account? <span onClick={()=>setCurrent("Login")}>Login here</span></p>
+                }
+            </form>
         </div>
     )
 }
